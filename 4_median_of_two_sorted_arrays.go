ec609a0edfb86e13a8c4012f518f6ca327a7e3ff
Tymofii Maksymenko <tim.off.max@gmail.com>
2020-07-31 08:15:01 +0300

    4. Median of Two Sorted Arrays

---
+++ 4_median_of_two_sorted_arrays.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"sort"
+)
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	result := 0.0
+
+	allNumbers := append(nums1, nums2...)
+	sort.Ints(allNumbers)
+
+	lenght := len(allNumbers)
+	middleIndex := (lenght - 1) / 2
+
+	if 0 == (len(allNumbers) % 2) {
+		medianSum := allNumbers[middleIndex] + allNumbers[middleIndex+1]
+		result = float64(medianSum) / 2
+	} else {
+		result = float64(allNumbers[middleIndex])
+	}
+
+	return result
+}
